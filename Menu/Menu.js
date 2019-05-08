d33042488b89f0bd4c15c50d9e53f761034212e6
Delphina Bailey Nicols <DelphinaBaileyNicols@gmail.com>
2019-05-07 20:15:31 -0400

    debugging menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,11 +1,18 @@
 
 const toggleMenu = () => {
+  const menuList = document.querySelectorAll("div ul li");
+  if (menu.style.display === "none") {
+      menu.style.display === "block";
+      menuList.style.display === "block";
+      
+  } 
   // Toggle the "menu--open" class on your menu refence. 
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelector('.menu');
+const menu = document.querySelectorAll('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', toggleMenu(menu));
+
+menuButton.addEventListener('click', toggleMenu());
